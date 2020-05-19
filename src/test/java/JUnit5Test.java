5041197b8179875ad3c38158840ddb9aa0bc78a4
aleksandrandreichenko <andreichenko@web.de>
2020-05-19 14:44:57 +0300

    feature/JAVA-test-juoiter-api created a first test for testing

--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -1,9 +1,19 @@
 import org.junit.jupiter.api.Test;
 public class JUnit5Test {
 
+    /**
+     *
+     *Simple test for application
+     */
     @Test
     void justAnTest(){
+       System.out.print("Test has been finished");
+    }
 
+    @Test
+    void secondTest(){
+        justAnTest();
+        System.out.print("the all tests was loaded");
     }
 
 }
