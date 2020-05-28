bbca7bf060450008f46f036b63cd69ab4c1c9f62
aleksandrandreichenko <andreichenko@web.de>
2020-05-28 16:19:49 +0300

    feature/JAVA-restpost-model
    added restController post
        - RestPostmodel + getters and setters
        - RestUser model + Address class + getters and setters with constructors
        - added Geo getters and setters with constructors

--- src/main/java/de/frei/springMvc/mvc/rest/model/RestUserModel.java
+++ src/main/java/de/frei/springMvc/mvc/rest/model/RestUserModel.java
@@ -69,10 +69,76 @@ public class RestUserModel {
         private String zipcode;
         private Geo geo;
 
+        public Address() {
+        }
 
+        public Address(String street, String suite, String zipcode, Geo geo) {
+            this.street = street;
+            this.suite = suite;
+            this.zipcode = zipcode;
+            this.geo = geo;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getSuite() {
+            return suite;
+        }
+
+        public void setSuite(String suite) {
+            this.suite = suite;
+        }
+
+        public String getZipcode() {
+            return zipcode;
+        }
+
+        public void setZipcode(String zipcode) {
+            this.zipcode = zipcode;
+        }
+
+        public Geo getGeo() {
+            return geo;
+        }
+
+        public void setGeo(Geo geo) {
+            this.geo = geo;
+        }
 
         public class Geo {
 
+            private Double lat;
+            private Double lng;
+
+            public Geo() {
+            }
+
+            public Geo(Double lat, Double lng) {
+                this.lat = lat;
+                this.lng = lng;
+            }
+
+            public Double getLat() {
+                return lat;
+            }
+
+            public void setLat(Double lat) {
+                this.lat = lat;
+            }
+
+            public Double getLng() {
+                return lng;
+            }
+
+            public void setLng(Double lng) {
+                this.lng = lng;
+            }
         }
 
     }
