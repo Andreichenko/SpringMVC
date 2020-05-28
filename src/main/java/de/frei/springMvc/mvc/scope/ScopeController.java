a2797611da833588df716b884333b27a12ba9eb6
aleksandrandreichenko <andreichenko@web.de>
2020-05-28 16:24:26 +0300

    feature/JAVA-scope-controller
    added scope controller
        - class with metadata
        - request mapping

--- src/main/java/de/frei/springMvc/mvc/scope/ScopeController.java
+++ src/main/java/de/frei/springMvc/mvc/scope/ScopeController.java
@@ -1,10 +1,21 @@
 package de.frei.springMvc.mvc.scope;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ScopeController {
 
-
+    @RequestMapping(value = "/scopeSession", method = RequestMethod.GET)
+    public ModelAndView scopeExample(HttpSession session) {
+        System.out.println("ScopeController scopeSession() is called");
+        session.setMaxInactiveInterval(3600);
+        session.setAttribute("sessionObject", "Value is session object");
+        return new ModelAndView("/scope/scope");
+    }
 
 }
