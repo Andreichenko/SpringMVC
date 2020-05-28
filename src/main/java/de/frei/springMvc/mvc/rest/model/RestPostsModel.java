7ab09a7ae6248f261b419e910e0beeb6c06d2a17
aleksandrandreichenko <andreichenko@web.de>
2020-05-28 15:27:29 +0300

    feature/JAVA-restpost-model
    added restController post
        - RestPostmodel

---
+++ src/main/java/de/frei/springMvc/mvc/rest/model/RestPostsModel.java
@@ -0,0 +1,9 @@
+package de.frei.springMvc.mvc.rest.model;
+
+public class RestPostsModel {
+
+    private String userId;
+    private String id;
+    private String title;
+    private String body;
+}
