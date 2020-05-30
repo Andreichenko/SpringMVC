98c0836a7afc7481a732c081f4cd15f79a3501e8
aleksandrandreichenko <andreichenko@web.de>
2020-05-30 16:31:48 +0300

    feature/JAVA-exception-controller-and-approach
    added scope controller
        - the main controller
        - a request mapping
        - exception resolver
        - Logger

--- src/main/java/de/frei/springMvc/mvc/exception/ExceptionHandler.java
+++ src/main/java/de/frei/springMvc/mvc/exception/ExceptionHandler.java
@@ -1,6 +1,8 @@
 package de.frei.springMvc.mvc.exception;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,12 +10,17 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @Component
 public class ExceptionHandler implements HandlerExceptionResolver {
 
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
     @Override
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        return null;
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                         Object o, Exception e) {
+        System.out.println("Spring MVC ExceptionHandler handling");
+        logger.error("ErrorLog: ", e);
+        return new ModelAndView("error/exception", "exceptionMsg", "ExceptionHandler msg: " + e.toString());
     }
 }
