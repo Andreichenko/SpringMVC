0afe439f1eb297a8e26af02dc2a9157050993219
aleksandrandreichenko <andreichenko@web.de>
2020-06-01 01:53:19 +0300

    feature/JAVA-security-controller
    added scope controller
        - security roles
        - spring security roles

--- src/main/java/de/frei/springMvc/mvc/security/SecurityController.java
+++ src/main/java/de/frei/springMvc/mvc/security/SecurityController.java
@@ -1,6 +1,7 @@
 package de.frei.springMvc.mvc.security;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,5 +18,14 @@ public class SecurityController {
     public ModelAndView adminOrSuperUserCanCall() {
         System.out.println("SecurityController adminOrSuperUserCanCall() is called");
         return new ModelAndView("/security/admin");
+    }
+
+    //Spring Security
+    //SpEL usage at method level security
+    @PreAuthorize("hasRole('ADMIN') || hasRole('SUPER_USER') || hasRole('USER')")
+    @RequestMapping(value="/userOrAdminCanCallSpEL", method=RequestMethod.GET)
+    public ModelAndView userOrAdminCanCall() {
+        System.out.println("SecurityController userOrAdminCanCall() is called with ROLE_ADMIN or ROLE_USER");
+        return new ModelAndView("/security/profile");
     }
 }
