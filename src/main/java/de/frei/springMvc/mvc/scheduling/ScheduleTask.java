ec85b916bd59fd4a52097ccaf5ebb8baca47726a
aleksandrandreichenko <andreichenko@web.de>
2020-06-02 23:51:36 +0300

    feature/JAVA-scheduling-controller
    added scope controller
        - new controller
        - schedule
        - task

--- src/main/java/de/frei/springMvc/mvc/scheduling/ScheduleTask.java
+++ src/main/java/de/frei/springMvc/mvc/scheduling/ScheduleTask.java
@@ -13,4 +13,10 @@ public class ScheduleTask {
     public void fixedDelaySchedule() {
         System.out.println("fixedDelaySchedule every 10 seconds" + new Date());
     }
+
+    //every 30 seconds (seconds, minutes, hours, day of month, month, day of week, year(optional))
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void cronSchedule() {
+        System.out.println("cronSchedule every 30 seconds" + new Date());
+    }
 }
