cee0c976a43c44dc540fdf094461715e7c735ddb
aleksandrandreichenko <andreichenko@web.de>
2020-06-02 23:51:05 +0300

    feature/JAVA-scheduling-controller
    added scope controller
        - new controller
        - schedule

--- src/main/java/de/frei/springMvc/mvc/scheduling/ScheduleTask.java
+++ src/main/java/de/frei/springMvc/mvc/scheduling/ScheduleTask.java
@@ -1,8 +1,16 @@
 package de.frei.springMvc.mvc.scheduling;
 
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+
+import java.util.Date;
 
 @Controller
 public class ScheduleTask {
+
+    @Scheduled(fixedDelay = 10000)
+    public void fixedDelaySchedule() {
+        System.out.println("fixedDelaySchedule every 10 seconds" + new Date());
+    }
 }
