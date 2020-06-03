cbd26bbe0813023d0549c3190858b0dba3a73b74
aleksandrandreichenko <andreichenko@web.de>
2020-06-03 23:38:44 +0300

    feature/JAVA-jstl-controller
    added scope controller
        - new controller
        - orm service
        - settings
        -query
        - context
        - insert user
        - delete user
        - ORM Controller
        - service
        - settings
        - findbyUserId
        - update in controller
        - delete user
        - insert user
        - Model and view

--- src/main/java/de/frei/springMvc/mvc/jstl/JSTLController.java
+++ src/main/java/de/frei/springMvc/mvc/jstl/JSTLController.java
@@ -3,6 +3,9 @@ package de.frei.springMvc.mvc.jstl;
 import de.frei.springMvc.mvc.orm.ORMService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JSTLController {
@@ -10,5 +13,8 @@ public class JSTLController {
     @Autowired
     private ORMService ormService;
 
-
+    @RequestMapping(value = "/jstlReturnUsers", method = RequestMethod.GET)
+    public ModelAndView listUsers() {
+        return new ModelAndView("jstl/jstl", "resultObjectList", ormService.queryFindAllUsersJPA());
+    }
 }
