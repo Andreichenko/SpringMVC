9e5df1738c0f17c5c39c26e20cbd5f75a4dadf7b
aleksandrandreichenko <andreichenko@web.de>
2020-06-03 23:34:34 +0300

    feature/JAVA-jstl-controller
    added scope controller
        - new controller
        - orm service
        - settings
        -query
        - context
        - insert user
        - delete user
        - ORM Controller
        - service

--- src/main/java/de/frei/springMvc/mvc/orm/ORMController.java
+++ src/main/java/de/frei/springMvc/mvc/orm/ORMController.java
@@ -1,10 +1,12 @@
 package de.frei.springMvc.mvc.orm;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ORMController {
 
-
+    @Autowired
+    private ORMService ormService;
 }
