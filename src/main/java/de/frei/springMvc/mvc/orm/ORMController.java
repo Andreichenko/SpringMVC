1dbf1e5783b2f42d874d418cd0595edc44639eeb
aleksandrandreichenko <andreichenko@web.de>
2020-06-03 23:35:12 +0300

    feature/JAVA-jstl-controller
    added scope controller
        - new controller
        - orm service
        - settings
        -query
        - context
        - insert user
        - delete user
        - ORM Controller
        - service
        - settings

--- src/main/java/de/frei/springMvc/mvc/orm/ORMController.java
+++ src/main/java/de/frei/springMvc/mvc/orm/ORMController.java
@@ -1,12 +1,25 @@
 package de.frei.springMvc.mvc.orm;
 
 
+import de.frei.springMvc.mvc.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ORMController {
 
     @Autowired
     private ORMService ormService;
+
+    @RequestMapping(value = "/ormFindAllUsers", method= RequestMethod.GET)
+    public ModelAndView ormFindAllUsers() {
+        System.out.println("ORMController ormFindAllUsers is called");
+        List<User> users = ormService.queryFindAllUsersJPA();
+        return new ModelAndView("/orm/orm", "resultObject", users);
+    }
 }
