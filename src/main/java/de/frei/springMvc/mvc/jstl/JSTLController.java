e8c0cdc2c8b668bcb144bc22d7dd73ac8c5be53d
aleksandrandreichenko <andreichenko@web.de>
2020-06-03 23:30:53 +0300

    feature/JAVA-jstl-controller
    added scope controller
        - new controller
        - orm service
        - settings

--- src/main/java/de/frei/springMvc/mvc/jstl/JSTLController.java
+++ src/main/java/de/frei/springMvc/mvc/jstl/JSTLController.java
@@ -1,5 +1,6 @@
 package de.frei.springMvc.mvc.jstl;
 
+import de.frei.springMvc.mvc.orm.ORMService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
--- src/main/java/de/frei/springMvc/mvc/orm/ORMService.java
+++ src/main/java/de/frei/springMvc/mvc/orm/ORMService.java
@@ -1,11 +1,14 @@
 package de.frei.springMvc.mvc.orm;
 
 
+import de.frei.springMvc.mvc.bean.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 @Transactional //need to update\delete queries. Don't forget <tx:annotation-driven/>
@@ -13,4 +16,11 @@ public class ORMService {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    public List<User> queryFindAllUsersJPA() {
+        System.out.println("ORMService queryfindAllUsersJPA is called");
+        String query = "from User order by iduser";
+        TypedQuery<User> typedQuery = entityManager.createQuery(query, User.class);
+        return typedQuery.getResultList();
+    }
 }
