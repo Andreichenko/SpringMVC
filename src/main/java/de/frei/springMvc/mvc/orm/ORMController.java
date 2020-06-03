448aa243c9d42aa032070cdc43b29c375cabc21b
aleksandrandreichenko <andreichenko@web.de>
2020-06-03 23:36:01 +0300

    feature/JAVA-jstl-controller
    added scope controller
        - new controller
        - orm service
        - settings
        -query
        - context
        - insert user
        - delete user
        - ORM Controller
        - service
        - settings
        - findbyUserId

--- src/main/java/de/frei/springMvc/mvc/orm/ORMController.java
+++ src/main/java/de/frei/springMvc/mvc/orm/ORMController.java
@@ -4,6 +4,7 @@ package de.frei.springMvc.mvc.orm;
 import de.frei.springMvc.mvc.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,5 +22,12 @@ public class ORMController {
         System.out.println("ORMController ormFindAllUsers is called");
         List<User> users = ormService.queryFindAllUsersJPA();
         return new ModelAndView("/orm/orm", "resultObject", users);
+    }
+
+    @RequestMapping(value = "/queryFindByIdUser/{userid}", method = RequestMethod.GET)
+    public ModelAndView queryFindByIdUser(@PathVariable("userid") int userid) {
+        System.out.println("ORMController queryFindByIdUser is called");
+        User user = ormService.queryFindUserById(userid);
+        return new ModelAndView("/orm/orm", "resultObject", user);
     }
 }
