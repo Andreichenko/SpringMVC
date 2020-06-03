55156af72127c7a088b68206e6564fc4cb6567eb
aleksandrandreichenko <andreichenko@web.de>
2020-06-03 23:29:04 +0300

    feature/JAVA-jstl-controller
    added scope controller
        - new controller
        - orm service

--- src/main/java/de/frei/springMvc/mvc/jstl/JSTLController.java
+++ src/main/java/de/frei/springMvc/mvc/jstl/JSTLController.java
@@ -1,8 +1,11 @@
 package de.frei.springMvc.mvc.jstl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class JSTLController {
 
+    @Autowired
+    private ORMService ormService;
 }
