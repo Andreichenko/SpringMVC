dd9e1986fb4984f0008527028dfae522cf841d0e
aleksandrandreichenko <andreichenko@web.de>
2020-06-04 16:26:36 +0300

    feature/JAVA-interceptors-controllers-and-models -initial

--- src/main/java/de/frei/springMvc/mvc/interceptors/SiteInterceptor.java
+++ src/main/java/de/frei/springMvc/mvc/interceptors/SiteInterceptor.java
@@ -1,4 +1,27 @@
 package de.frei.springMvc.mvc.interceptors;
 
-public class SiteInterceptor {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class SiteInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
 }
