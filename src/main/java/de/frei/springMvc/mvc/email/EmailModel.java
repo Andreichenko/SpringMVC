8520ae2155963b1eab2b2b107c0c71e31f7a5866
aleksandrandreichenko <andreichenko@web.de>
2020-06-06 15:25:04 +0300

    feature/JAVA-email-service -initial added forms settings - model and controller

--- src/main/java/de/frei/springMvc/mvc/email/EmailModel.java
+++ src/main/java/de/frei/springMvc/mvc/email/EmailModel.java
@@ -1,4 +1,10 @@
 package de.frei.springMvc.mvc.email;
 
 public class EmailModel {
+
+    private String name;
+    private String email;
+    private String password;
+    private String message;
+
 }
