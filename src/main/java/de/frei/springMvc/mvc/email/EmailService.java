801fd94651d44408d96f357543562b4f961f909d
aleksandrandreichenko <andreichenko@web.de>
2020-06-06 15:29:09 +0300

    feature/JAVA-email-service -initial added forms settings - model and controller - getter and setters - work with controller - added some atributes - final fix

--- src/main/java/de/frei/springMvc/mvc/email/EmailService.java
+++ src/main/java/de/frei/springMvc/mvc/email/EmailService.java
@@ -3,10 +3,14 @@ package de.frei.springMvc.mvc.email;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -28,7 +32,47 @@ public class EmailService {
 
     @Autowired
     private VelocityEngine velocityEngine; //see application-context.xml
+    public boolean sendEmail (final String templateName, final Map<String, Object> model) {
+        boolean res = false;
+        try {
+            MimeMessagePreparator preparator = new MimeMessagePreparator() {
 
+                @Override
+                public void prepare(MimeMessage mimeMessage) throws Exception {
+                    String from = (String) model.get(FROM);
+                    String to = (String) model.get(TO);
+                    String subject = (String) model.get(SUBJECT);
+
+                    List<String> bccList = (List<String>) model.get(BCC_LIST);
+
+                    MimeMessageHelper message = new MimeMessageHelper(mimeMessage, "UTF-8"); //ENCODING IMPORTANT!
+                    message.setFrom(from);
+                    message.setTo(to);
+                    message.setSubject(subject);
+                    message.setSentDate(new Date());
+                    if (bccList != null) {
+                        for (String bcc : bccList) {
+                            message.addBcc(bcc);
+                        }
+                    }
+
+                    model.put("noArgs", new Object());
+                    String text = VelocityEngineUtils.mergeTemplateIntoString(
+                            velocityEngine, templateName, "UTF-8", model);
+
+                    message.setText(text,true);
+                }
+            };
+
+            mailSender.send(preparator);
+            res = true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return res;
+    }
 
 
 }
