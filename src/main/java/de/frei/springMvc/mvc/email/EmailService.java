fe6d377bb28860c5efbcf77c39789ca895f17144
aleksandrandreichenko <andreichenko@web.de>
2020-06-06 15:21:41 +0300

    feature/JAVA-email-service -initial added forms

--- src/main/java/de/frei/springMvc/mvc/email/EmailService.java
+++ src/main/java/de/frei/springMvc/mvc/email/EmailService.java
@@ -1,7 +1,19 @@
 package de.frei.springMvc.mvc.email;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
-@Controller
+@Service
 public class EmailService {
+
+    /*Email From*/
+    public static final String FROM = "from";
+    /*Email To*/
+    public static final String TO = "to";
+    /*Email Subject*/
+    public static final String SUBJECT = "subject";
+    /*Email BCC*/
+    public static final String BCC_LIST = "bccList";
+    /*Email CCC*/
+    public static final String CCC_LIST = "ccList";
+
 }
