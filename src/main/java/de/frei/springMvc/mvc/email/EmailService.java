65158d54a16119b01d1af2492e593fa21d24dd49
aleksandrandreichenko <andreichenko@web.de>
2020-06-06 15:23:36 +0300

    feature/JAVA-email-service -initial added forms settings

--- src/main/java/de/frei/springMvc/mvc/email/EmailService.java
+++ src/main/java/de/frei/springMvc/mvc/email/EmailService.java
@@ -2,8 +2,10 @@ package de.frei.springMvc.mvc.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
+import javax.mail.internet.MimeMessage;
 import java.util.Map;
 
 @Service
@@ -27,4 +29,12 @@ public class EmailService {
     public boolean sendEmail (final String templateName, final Map<String, Object> model) {
         boolean res = false;
     }
-}
+     try {
+        MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            @Override
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+                String from = (String) model.get(FROM);
+                String to = (String) model.get(TO);
+                String subject = (String) model.get(SUBJECT);
+
+        }
