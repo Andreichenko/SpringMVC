6ba8caaf8af259b8a2f8a2824946df7798d272f1
aleksandrandreichenko <andreichenko@web.de>
2020-06-08 21:22:14 +0300

    feature/JAVA-cron-tab ----initial-- - some task

---
+++ src/main/java/de/frei/springMvc/mvc/quartz/QuartzTask.java
@@ -0,0 +1,13 @@
+package de.frei.springMvc.mvc.quartz;
+
+
+/**
+ * look application-context.xml
+ * 1. simpleTrigger
+ * 2. simpleQuartzJob
+ * 3. bean id="simpleQuartzTask"
+ * 4. Quartz Scheduler
+ */
+
+public class QuartzTask {
+}
