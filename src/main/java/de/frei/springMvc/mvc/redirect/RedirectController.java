af2f46cf087186815957fc24c1845b40e371d879
aleksandrandreichenko <andreichenko@web.de>
2020-06-09 15:49:32 +0300

    feature/JAVA-redirect-controller added - initialize commit

--- src/main/java/de/frei/springMvc/mvc/redirect/RedirectController.java
+++ src/main/java/de/frei/springMvc/mvc/redirect/RedirectController.java
@@ -1,12 +1,15 @@
 package de.frei.springMvc.mvc.redirect;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RedirectController {
     //redirect to external URL
+    @RequestMapping(value = "/redirectExample", method = RequestMethod.GET)
     public String redirectExample(HttpServletRequest request) {
         return "redirect:" + request.getScheme() +"://jenkins.sytes.net";
     }
