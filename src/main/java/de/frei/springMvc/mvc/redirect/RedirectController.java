a7efcba9eb452f2b296f5362da96d915f7512373
aleksandrandreichenko <andreichenko@web.de>
2020-06-09 15:48:30 +0300

    feature/JAVA-redirect-controller added - initialize commit

--- src/main/java/de/frei/springMvc/mvc/redirect/RedirectController.java
+++ src/main/java/de/frei/springMvc/mvc/redirect/RedirectController.java
@@ -2,7 +2,12 @@ package de.frei.springMvc.mvc.redirect;
 
 import org.springframework.stereotype.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class RedirectController {
     //redirect to external URL
+    public String redirectExample(HttpServletRequest request) {
+
+    }
 }
